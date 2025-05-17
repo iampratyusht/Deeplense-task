38132d859ab2762cc395d6148585acfe6b0d1cc3
iampratyusht <iampratyusht>
2025-05-17 20:09:56 +0530

    Changes in config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+
 CONFIG = {
-    "data_path": "/path/to/dataset",           # Root path to the dataset
-    "pretraining_label":"no_sub",              # Options: "cdm", "axion"
-    "pretrain": False,                         # True for pretraining, False for downstream tasks
-    "downstream": "super-resolution",          # Options: "classifier", "super-resolution", or None
-    "batch_size": 256,                         # Batch size for DataLoader
-    "split_ratio": 0.9,                        # Train/test split ratio
+    "data_path": Path("data/"),           # Root folder for your dataset
+    "pretrain": False,                    # Set True for self-supervised pretraining mode
+    "pretraining_label": "pretrain",      # Folder name under data_path for pretraining .npy files
+    "downstream": "classifier",           # Options: 'classifier', 'super-resolution'
+    "mode": "train",                      # Options: 'train', 'test'
+    "batch_size": 32,                     # Batch size for dataloaders
+    "split_ratio": 0.8,                   # Train/Val split ratio
+    "num_workers": 4,                     # DataLoader workers
+    "pin_memory": True,                   # DataLoader pin memory for GPU acceleration
 }
